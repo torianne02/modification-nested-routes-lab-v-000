f4645ba164b6aa1a766a947acd4e2115cc8ad9bd
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-24 19:38:22 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -8,4 +8,6 @@ module ArtistsHelper
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
 end
